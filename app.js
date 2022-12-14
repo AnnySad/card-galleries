e01223fe4f9ba56bc4bdca1318c0d6e06a76499e
AnnySad <anna.sadovskaya555@gmail.com>
2022-12-14 18:34:02 +0300

    fix-activeSlide

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const slides = document.querySelectorAll(
-    '.slide'
-)
 
-let slides1 = slides;
-for (const slide of slides1){
-    slide.addEventListener('click', ()=>{
-        clearActiveClasses()
 
-        slide.classList.add('active')
-    })
+function slidesPlugin(activeSlide = 0) {
+    const slides = document.querySelectorAll(
+        '.slide'
+    )
+
+    slides[activeSlide].classList.add('active')
+
+    let slides1 = slides;
+    for (const slide of slides1){
+        slide.addEventListener('click', ()=>{
+            clearActiveClasses()
+
+            slide.classList.add('active')
+        })
+    }
+
+    function clearActiveClasses(){
+        slides.forEach((slide)=>{
+            slide.classList.remove('active')
+        })
+    }
 }
-
-function clearActiveClasses(){
-    slides.forEach((slide)=>{
-        slide.classList.remove('active')
-    })
-}
+slidesPlugin(1)
